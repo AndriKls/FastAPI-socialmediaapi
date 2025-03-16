c1ba885050814a5a4779b4dc5af1caf5ad9896ae
AndriKls <158137294+AndriKls@users.noreply.github.com>
2025-03-16 21:22:47 +0200

    Refactor

--- SocialMediaAPI/main.py
+++ SocialMediaAPI/main.py
@@ -1,21 +1,8 @@
 from fastapi import FastAPI
-from models.posts import UserPost, UserPostIn
+
+from SocialMediaAPI.routers.post import router as post_router
 
 app = FastAPI()
 
 
-post_table = {}
-
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.model_dump()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+app.include_router(post_router)
